5f099fef2272295962b38de2e2cd5470df668810
Golden_Reflection <reflectiongolden@gmail.com>
2026-01-30 11:22:32 +0000

    Added and implemented password generator callback function.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './App.css'
+import PasswordGenerator from './components/PasswordGenerator';
 
 import {faGithub} from '@fortawesome/free-brands-svg-icons';
 
 function App() {
+  const [length,setLength] = useState(10);
+  const [numAllowed,setNumAllowed] = useState(false);
+  const [specCharAllowed,setSpecCharAllowed] = useState(false);
+  const [password,setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let strData = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numAllowed) {strData += "0123456789"}
+    if (specCharAllowed) {strData += "!£$%&@#~+-=?_"}
+    for (let index = 0; index < length; index++) {
+      let char = Math.floor(Math.random()*strData.length+1);
+      pass += strData.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numAllowed, specCharAllowed, setPassword]);
 
   return (
     <>
       <h1>Password Generator Application</h1>
       <p>This project has been made in conjunction with the <a title='GitHub Pages site' href="https://reflectiongolden.github.io/react-essentials-site/" target='_blank'>react-essentials-site</a><a title='GitHub Repository' href="https://github.com/ReflectionGolden/react-essentials-site" target='_blank'><FontAwesomeIcon icon={faGithub} /></a> project as part of Module 8.</p>
       <Container>
-        <h2>Password Generator</h2>
-        <p>Lorem ipsum dolor sit amet.</p>
+        <PasswordGenerator length={length} numAllowed={numAllowed} specCharAllowed={specCharAllowed} password={password} setLength={setLength} setNumAllowed={setNumAllowed} setSpecCharAllowed={setSpecCharAllowed} passwordGenerator={passwordGenerator} />
       </Container>
     </>
   )
--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function PasswordGenerator({length, numAllowed, specCharAllowed, password, setLength, setNumAllowed, setSpecCharAllowed, setPassword}) {
+function PasswordGenerator({length, numAllowed, specCharAllowed, password, setLength, setNumAllowed, setSpecCharAllowed, passwordGenerator}) {
     return(
         <div className="container mx-auto my-8">
           <h1 className="mb-8">Password Generator</h1>
@@ -56,9 +56,10 @@ function PasswordGenerator({length, numAllowed, specCharAllowed, password, setLe
                   <span className="ml-2">Include Special Characters</span>
                 </label>
               </div>
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                Generate Password
-              </button>
+              <button 
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
+                type="button" 
+                onClick={passwordGenerator}>Generate Password</button>
             </div>
           </div>
         </div>
